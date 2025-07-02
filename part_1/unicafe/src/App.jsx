93a4aa26c3cc74393703084b2d5c12da9ac7151e
580dhruv <dhruvishpatel580@gmail.com>
2025-07-03 00:45:54 +0530

    part_1 : unicafe-1.7 implemented

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -21,23 +21,51 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0)
 
-  const handleGood = () => setGood(good+1)
-  const handleNeutral = () => setNeutral(neutral+1)
-  const handleBad = () => setBad(bad+1)
-
+  const handleGood = () => {
+    setGood(good+1)
+    setTotal(total+1)
+    setAvg(avg+1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setTotal(total+1)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+    setTotal(total+1)
+    setAvg(avg-1)
+  }
+  if(total!==0) {
+    return (
+      <div>
+        <Heading title='give feedback'/>
+        <Button onClick={handleGood} text='good'/>
+        <Button onClick={handleNeutral} text="neutral"></Button>
+        <Button onClick={handleBad} text="bad"></Button>
+        <Heading title='statistics'/>
+        <Statistics stat_type='good' stats={good}/>
+        <Statistics stat_type='neutral' stats={neutral}/>
+        <Statistics stat_type='bad' stats={bad}/>
+        <Statistics stat_type='all' stats={total}/>
+        <Statistics stat_type='average' stats={avg/total}/>
+        <Statistics stat_type='positive' stats={(good/total)*100+' %'}/>
+      </div>
+    )
+  } 
   return (
     <div>
-      <Heading title='give feedback'/>
-      <Button onClick={handleGood} text='good'/>
-      <Button onClick={handleNeutral} text="neutral"></Button>
-      <Button onClick={handleBad} text="bad"></Button>
-      <Heading title='statistics'/>
-      <Statistics stat_type='good' stats={good}/>
-      <Statistics stat_type='neutral' stats={neutral}/>
-      <Statistics stat_type='bad' stats={bad}/>
-    </div>
+        <Heading title='give feedback'/>
+        <Button onClick={handleGood} text='good'/>
+        <Button onClick={handleNeutral} text="neutral"></Button>
+        <Button onClick={handleBad} text="bad"></Button>
+        <Heading title='statistics'/>
+        No feedback given
+      </div>
   )
+  
 }
 
 export default App
